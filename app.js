9dc9a78ff03fc736f8d844ee63870e33ac18f584
Angelina Gesch <angelina.gesch@gmx.de>
2023-11-28 09:02:32 +0100

    Website Frame und Highscore Board

---
+++ app.js
@@ -0,0 +1,77 @@
+var express = require('express');
+var mysql = require('mysql');
+var ejs = require('ejs');
+var bodyParser = require('body-parser');
+var expressLayouts = require('express-ejs-layouts')
+
+var app = express();
+var port = 3000;
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "test"
+});
+
+con.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected to MySQL database");
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/js', express.static(__dirname + '/js'));
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/assets', express.static(__dirname + '/assets'));
+app.use(expressLayouts);
+
+
+app.set('layout', 'layout'); // Set the default layout
+app.set('view engine', 'ejs');
+
+
+
+//Get Index Page
+app.get('/', function(req, res) {
+  res.render('index', {pageTitle: "Home"});
+});
+
+//Get Highscore Page
+app.get('/highscore', function (req, res) {
+  
+  var selectedKurs = req.query.selectedKurs || 'Alle'; // Get the selected Kurs from the form
+  if(selectedKurs === 'Alle'){
+    var userQuery = "SELECT vorname, name, COALESCE(SUM(exp), 0) AS exp " +
+              "FROM test.user " +
+              "LEFT JOIN test.user_kurs USING (user_id)" +
+              "GROUP BY user_id ORDER BY exp DESC";
+  }else{
+    var userQuery = "SELECT vorname, name, COALESCE(SUM(exp), 0) AS exp " +
+              "FROM test.user " +
+              "LEFT JOIN test.user_kurs USING (user_id)" +
+              "LEFT JOIN test.kurs USING (kurs_id)" +
+              "WHERE bezeichnung = '" + selectedKurs +
+              "' GROUP BY user_id ORDER BY exp DESC";
+  }
+  
+
+    
+    var kursQuery = "SELECT bezeichnung FROM test.kurs";
+
+    con.query(userQuery, function (errUser, userRows) { // Datenbankabfrage Userscores
+        if (errUser) throw errUser;
+    
+        con.query(kursQuery, function (errKurs, kursRows) { // Datenbankabfrage Kurse für Kursselect
+          if (errKurs) throw errKurs;
+          
+          // Render the 'highscore.ejs' template with both sets of data
+          res.render('highscore', { userData: userRows, kursData: kursRows, selectedKurs: selectedKurs, pageTitle: "Highscore-Board"});
+        });
+      });
+
+});
+
+app.listen(port, function () {
+  console.log("Server is running on port " + port);
+});
+
---
+++ js/highscore.js
@@ -0,0 +1,72 @@
+//Kurs Select
+document.addEventListener('DOMContentLoaded', function () {
+    var dropdown = document.getElementById('kursSelect');
+
+    dropdown.addEventListener('click', function (event) {
+      if (event.target.classList.contains('dropdown-item')) {
+        var selectedKurs = event.target.innerText.trim();
+        window.location.href = '/highscore?selectedKurs=' + encodeURIComponent(selectedKurs);
+      }
+    });
+  });
+
+document.addEventListener('DOMContentLoaded', function () {
+  const content = document.querySelector('.content'); // Get the table
+  const itemsPerPage = 10;
+  let currentPage = 0;
+  const items = Array.from(content.getElementsByTagName('tr')).slice(1); // Get all rows in the table
+
+  function showPage(page) {
+      const startIndex = page * itemsPerPage;
+      const endIndex = startIndex + itemsPerPage;
+      items.forEach((item, index) => {
+          item.toggleAttribute('hidden', index < startIndex || index >= endIndex);
+      });
+      updateButtonStates();
+  }
+
+  function createPageButtons() {
+      const totalPages = Math.ceil(items.length / itemsPerPage);
+
+      // Add Pagination div
+      const paginationDiv = document.createElement('div');   
+      paginationDiv.classList.add('pagination');
+      document.getElementById('container').appendChild(paginationDiv);
+      
+      // Add "Previous" button
+      const prevButton = document.createElement('button');
+      prevButton.textContent = '<';
+      prevButton.addEventListener('click', () => {
+          if (currentPage > 0) {
+              currentPage--;
+              showPage(currentPage);
+          }
+      });
+      paginationDiv.appendChild(prevButton);
+
+      // Add "Next" button
+      const nextButton = document.createElement('button');
+      nextButton.textContent = '>';
+      nextButton.addEventListener('click', () => {
+          if (currentPage < totalPages - 1) {
+              currentPage++;
+              showPage(currentPage);
+          }
+      });
+      paginationDiv.appendChild(nextButton);
+      
+      updateButtonStates(); // Set initial button states
+  }
+
+  function updateButtonStates() {
+      const totalPages = Math.ceil(items.length / itemsPerPage);
+      const prevButton = document.querySelector('.pagination button:first-child');
+      const nextButton = document.querySelector('.pagination button:last-child');
+
+      prevButton.disabled = currentPage === 0;
+      nextButton.disabled = currentPage === totalPages - 1;
+  }
+
+  createPageButtons(); // Call this function to create the Next and Previous buttons initially
+  showPage(currentPage);
+});
