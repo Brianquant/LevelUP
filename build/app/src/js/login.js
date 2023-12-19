0b88473a7bc188249d9d6348c3c8956cc3671df0
Vladimir <vladimir.hoffmann@outlook.de>
2023-12-19 10:38:14 +0100

    Added Authentication and Authorization (initiated changes to MVC struckture)

---
+++ build/app/src/js/login.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var form = document.querySelector('form');
+    form.addEventListener('submit', function (event) {
+        event.preventDefault(); // Prevent the default form submission
+        var username = document.getElementById('login-username').value;
+        var password = document.getElementById('login-pwd').value;
+        // Call your authentication function here
+        authenticateUser(username, password);
+    });
+
+    function authenticateUser(username, password) {
+        fetch('/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ username, password }),
+        })
+        .then(response => {
+            if (response.ok) {
+                // Authentication successful
+                window.location.href = '/'; // Redirect to the index page
+            } else {
+                // Authentication failed
+                return response.json();
+            }
+        })
+        .catch(error => {
+            console.error('Authentication failed:', error);
+        });
+    }
+});
