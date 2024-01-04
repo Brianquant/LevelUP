ed5a8cf6630e4d0b50679557d577de09d2248385
Vladimir <vladimir.hoffmann@outlook.de>
2024-01-04 07:38:22 +0100

    Added logic for handling a case in which the username already exists in the database

--- build/app/src/js/signup.js
+++ build/app/src/js/signup.js
@@ -22,7 +22,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 {
                     $('#passwordSignup').removeClass('is-invalid');
                     $('#passwordSignup').addClass('is-valid');
-                    $('#passwordCheckSignup').removeClass('is-valid');
+                    $('#passwordCheckSignup').removeClass('is-invalid');
                     $('#passwordCheckSignup').addClass('is-valid');
                 }
                 else{
@@ -36,4 +36,17 @@ document.addEventListener('DOMContentLoaded', function () {
             }, false)
         })
     })()
+
+    /*var form = document.querySelector('form');
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+        var password = document.getElementById('passwordSignup');
+        var passwordChek = document.getElementById('passwordCheckSignup');
+        
+        if (password.value != passwordChek.value) {
+            alert('Passwörter stimmen nicht überein');
+        } else {
+            form.submit();
+        }
+    });*/
 });
