97c2cd6883a4c6c5b0daabaa4ebcb45ecc9a1bd5
Vladimir <vladimir.hoffmann@outlook.de>
2024-01-04 07:38:22 +0100

    Added logic for handling a case in which the username already exists in the database

--- build/app/src/routes/signup-routes.js
+++ build/app/src/routes/signup-routes.js
@@ -11,6 +11,7 @@ router.post('/signup', urlencodedParser, async (req, res) => {
         firstname: req.body.firstname,
         lastname: req.body.lastname,
         grade: req.body.grade,
+        username: req.body.firstname.slice(0, 9) + '_' + req.body.lastname.slice(0, 9),
         password: await bcrypt.hash(req.body.password, 12),
     };
     
@@ -18,14 +19,20 @@ router.post('/signup', urlencodedParser, async (req, res) => {
     const connection = await pool.getConnection();
 
     // write user into database
-    
-    const [user] = await connection.query('INSERT INTO benutzer(name, vorname, passwort, klasse_id) VALUES(?, ?, ?, ?)', 
-    [new_user.lastname], [new_user.firstname], [new_user.grade], [new_user.password]);
+    const userCount = await connection.query('SELECT COUNT(*) AS count FROM benutzer WHERE name = ? AND vorname = ?', [new_user.lastname, new_user.firstname]);
+    //console.log(new_user.username, userCount[0][0].count);
+
+    if (userCount[0][0].count > 0) {
+        const [user] = await connection.query('INSERT INTO benutzer(name, vorname, klasse_id, passwort, username) VALUES(?, ?, ?, ?, ?)', 
+        [new_user.lastname, new_user.firstname, new_user.grade, new_user.password, new_user.firstname.slice(0, 9) + '_' + new_user.lastname.slice(0, 9) + userCount[0][0].count]);
+    } else {
+        const [user] = await connection.query('INSERT INTO benutzer(name, vorname, klasse_id, passwort, username) VALUES(?, ?, ?, ?, ?)', 
+        [new_user.lastname, new_user.firstname, new_user.grade, new_user.password, new_user.username]);
+    }
 
     // Close the connection
     connection.release();
-
-    console.log(new_user);
+    res.redirect('/login');
 });
 
 module.exports = router;
